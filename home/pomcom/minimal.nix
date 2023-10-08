dfc82555a1413168909b07f0d1e007f783af6795
pomcom <git@pomcom.email>
2023-10-08 14:59:08 +0200

    fix missing package

--- home/pomcom/minimal.nix
+++ home/pomcom/minimal.nix
@@ -7,6 +7,13 @@
       ./features/desktop/common/font.nix
       ./features/desktop/common/wayland-wm/alacritty.nix
 
+  home = {
+    username = "pomcom";
+    homeDirectory = /home/pomcom;
+
+
+    };
+
   home.packages = with pkgs; [
 
   ];
