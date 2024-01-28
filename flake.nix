a14056e306e74f166abee8b3bfda8abef469bc4c
pomcom <git@pomcom.email>
2024-01-28 22:04:54 +0100

    add stuff

--- flake.nix
+++ flake.nix
@@ -93,7 +93,7 @@
           };
 
           "pomcom@kali" = home-manager.lib.homeManagerConfiguration {
-            defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+            # defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
             extraSpecialArgs = { inherit inputs outputs; };
             modules = [ ./home/pomcom/kali.nix ];
