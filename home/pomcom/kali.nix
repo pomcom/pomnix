973e64e85e17c02b11d3d661bd6493fc44bc44c4
pomcom <git@pomcom.email>
2024-01-29 07:49:46 +0100

    heureka

--- home/pomcom/kali.nix
+++ home/pomcom/kali.nix
@@ -1,19 +1,17 @@
 { inputs, pkgs, ... }: {
 
-
+  
+ {
   imports = [
 
     ./global
     ./features/desktop/common/font.nix
     ./features/desktop/common/wayland-wm/alacritty.nix
-
-    home = {
-    username = "pomcom";
-    homeDirectory = /home/pomcom;
+  ];
 
 
-  };
 
     home.packages = with pkgs; [
 
   ];
+}
