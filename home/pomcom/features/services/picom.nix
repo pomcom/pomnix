ffaad604fa2d156288d38b94145dc45083bc084e
pomcom <git@pomcom.email>
2024-05-25 11:02:25 +0200

    adjust picom

--- home/pomcom/features/services/picom.nix
+++ home/pomcom/features/services/picom.nix
@@ -3,15 +3,15 @@
   services.picom = {
     enable = true;
     vSync = true;
-    #backend = "glx";
-    /* activeOpacity = 0.7; */
-    # inactiveOpacity = 0.9;
+#backend = "glx";
+    # activeOpacity = 0.95; 
+    # inactiveOpacity = 0.7;
     settings = {
-      # "corner-radius" = 1;
-      # "round-borders" = 1;
+"corner-radius" = 5;
+"round-borders" = 1;
       "shadow" = true;
-      # "fading" = true;
-      # "fade-in-step" = 0.3;
+# "fading" = true;
+"fade-in-step" = 0.3;
 
     };
 
